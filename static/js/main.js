7655cecb842bb380c78ff160364699db345325bc
Misaka13514 <Misaka13514@gmail.com>
2021-11-04 23:19:24 +0800

    Modify web page

--- static/js/main.js
+++ static/js/main.js
@@ -221,3 +221,4 @@ function Init(feedType, feedPath, bgArr, senArr) {
     });
 }
 console.log("\n %c Present %c By Zapic \n\n", "color: #fff; background: #fb7299; padding:5px 0;", "background: #efefef; padding:5px 0;");
+console.log("\n %c Modified %c By Misaka13514 \n\n", "color: #fff; background: #fb7299; padding:5px 0;", "background: #efefef; padding:5px 0;");
